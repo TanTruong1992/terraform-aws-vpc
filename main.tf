5d2f4bad6e0fdf50b003f7d0952a8d873c9236d9
TanTruong1992 <50537207+TanTruong1992@users.noreply.github.com>
2024-07-21 22:52:22 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,51 +1,93 @@
-data "aws_availability_zones" "available" {}
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = true
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.12.0"
-  
-  name    = "${var.project}-vpc"
-  cidr    = var.vpc_cidr
-  azs     = data.aws_availability_zones.available.names
-
-  private_subnets  = var.private_subnets
-  public_subnets   = var.public_subnets
-  database_subnets = var.database_subnets
-
-  create_database_subnet_group = true
-  enable_nat_gateway           = true
-  single_nat_gateway           = true
+  tags = {
+    "Name" = "custom"
+  }
 }
 
-module "lb_sg" {
-  source = "terraform-in-action/sg/aws"
-  vpc_id = module.vpc.vpc_id
-  ingress_rules = [
-    {
-      port        = 80
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
+resource "aws_subnet" "private_subnet" {
+  count = length(var.private_subnet)
+
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private_subnet[count.index]
+  availability_zone = var.availability_zone[count.index % length(var.availability_zone)]
+
+  tags = {
+    "Name" = "private-subnet"
+  }
 }
 
-module "web_sg" {
-  source = "terraform-in-action/sg/aws"
-  vpc_id = module.vpc.vpc_id
-  ingress_rules = [
-    {
-      port        = 80
-      security_groups = [module.lb_sg.security_group.id]
-    }
-  ]
+resource "aws_subnet" "public_subnet" {
+  count = length(var.public_subnet)
+
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.public_subnet[count.index]
+  availability_zone = var.availability_zone[count.index % length(var.availability_zone)]
+
+  tags = {
+    "Name" = "public-subnet"
+  }
 }
 
-module "db_sg" {
-  source = "terraform-in-action/sg/aws"
-  vpc_id = module.vpc.vpc_id
-  ingress_rules = [
-    {
-      port            = 5432
-      security_groups = [module.web_sg.security_group.id]
-    }
-  ]
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    "Name" = "custom"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+
+  tags = {
+    "Name" = "public"
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  for_each       = { for k, v in aws_subnet.public_subnet : k => v }
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "public" {
+  depends_on = [aws_internet_gateway.ig]
+
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet[0].id
+
+  tags = {
+    Name = "Public NAT"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.public.id
+  }
+
+  tags = {
+    "Name" = "private"
+  }
+}
+
+resource "aws_route_table_association" "public_private" {
+  for_each       = { for k, v in aws_subnet.private_subnet : k => v }
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
 }
